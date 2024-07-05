13deb888eb83c38ba25ad2e4c539521f8b25b354
Omelihunna <omelihunna@hotmail.com>
2024-07-05 10:14:12 +0100

    Testimonials Fix

--- src/components/sections/LandingPage/Testimonials.tsx
+++ src/components/sections/LandingPage/Testimonials.tsx
@@ -6,7 +6,7 @@ const Testimonials: React.FC = () => {
         <div className="flex flex-col relative py-[56px] justify-center px-[120px]">
             <img
                 src="hands-of-teenagers-showing-okay-sign.png"
-                className="w-[518px] h-[345px] absolute right-0 top-0 opacity-40 object-cover"
+                className="md:w-[518px] md:h-[345px] absolute right-0 top-0 opacity-40 object-cover"
             />            
             <div className="flex flex-col justify-start items-center relative gap-6">
                 <p className="text-xl font-bold text-left text-[#946108]">
@@ -16,7 +16,7 @@ const Testimonials: React.FC = () => {
                     <p className="text-[32px] font-bold text-left text-black">
                         Real feedback, Real Results!
                     </p>
-                    <p className="w-[641px] opacity-[0.92] text-lg text-center text-black">
+                    <p className="opacity-[0.92] text-lg text-center text-black">
                         Discover the impact of our services through authentic feedback from satisfied clients. Real
                         experienes, real improvemnts, real results.
                     </p>
@@ -29,7 +29,7 @@ const Testimonials: React.FC = () => {
             </div>
             <img
                 src="hands-of-teenagers-showing-okay-sign.png"
-                className="w-[518px] h-[345px] absolute bottom-0 left-0 rotate-180 opacity-40 object-cover"
+                className="md:w-[518px] md:h-[345px] absolute bottom-0 left-0 rotate-180 opacity-40 object-cover"
             />
         </div>
     )
